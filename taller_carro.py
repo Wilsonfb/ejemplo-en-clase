62638a03a15bfbb941976f8981d9e79ea0d82546
Wilsonfb <150964778+Wilsonfb@users.noreply.github.com>
2024-04-25 16:45:42 -0500

    Update taller_carro.py

---
+++ taller_carro.py
@@ -0,0 +1,13 @@
+#crear una clase carro con 4 atributos, crear dos objetos de clase.
+
+class Carro():
+    Marca="Toyota-"
+    Tipo="4X4-"
+    Combustible="Gasolina extra."
+    Sillas="De cuero."
+Carro1=Carro
+Carro2=Carro
+Carro3=Carro
+Carro4=Carro
+print(Carro1.Marca)
+print(Carro2.Tipo)
