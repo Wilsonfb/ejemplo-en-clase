495d7211656bb1abcd01dcec9a9b258c9af66be5
Wilsonfb <150964778+Wilsonfb@users.noreply.github.com>
2024-05-11 15:30:24 -0500

    Update taller_4.py

--- taller_4.py
+++ taller_4.py
@@ -1,11 +1,11 @@
 #Crear una clase llamada rectangulo con los parametros de base y altura con un objeto que sea calcular_area.
 class Rectngulo:
-    def _init_(self,base,altura):
-        self.base=base
-        self.altura=altura
+    def __init__ (self,base,altura):
+        self.base = base
+        self.altura = altura
     def calcular_area(self):
-        print(f"El area del rectangulo es: {altura*base}")
-altura=float(input("Digite la altura: "))
-base=float(input("Digite la base: "))
-rectangulo1=Rectngulo()
+        print(f"El area del rectangulo es: {self.altura * self.base}.")
+altura = float(input("Digite la altura: "))
+base = float(input("Digite la base: "))
+rectangulo1 = Rectngulo(altura, base)
 rectangulo1.calcular_area()
